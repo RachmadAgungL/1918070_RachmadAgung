f8f194ddbd8f90c9687425a937d241e5b541062d
Ahmad Faisol <mzfais@gmail.com>
2019-10-08 10:39:26 +0700

    Tambah layout profile 2

--- app/src/main/java/id/ac/itn/myprofile/MainActivity.java
+++ app/src/main/java/id/ac/itn/myprofile/MainActivity.java
@@ -10,7 +10,7 @@ import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
-    Button btn;
+    Button btn,btn2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +18,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
         btn = findViewById(R.id.btnLayout);
         btn.setOnClickListener(this);
+        btn2 = findViewById(R.id.btnLayout2);
+        btn2.setOnClickListener(this);
     }
 
     @Override
@@ -25,6 +27,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         if(view.getId()==R.id.btnLayout){
             Log.d("MainActivity", "onClick: gak tampil bosss");
             Intent intent = new Intent(MainActivity.this,LayoutGroupActivity.class);
+            startActivity(intent);
+        }
+        if(view.getId()==R.id.btnLayout2){
+            Log.d("MainActivity", "onClick: gak tampil bosss");
+            Intent intent = new Intent(MainActivity.this,LayoutGroup2Activity.class);
             startActivity(intent);
         }
     }
